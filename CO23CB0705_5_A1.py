5f87d681fe652e9a5b316a166f4b3b5900bcd96e
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-24 13:21:35 +0530

    Update CO23CB0705_5_A1.py

--- CO23CB0705_5_A1.py
+++ CO23CB0705_5_A1.py
@@ -1,1 +1,34 @@
+# Example dictionary
+my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
 
+# Extract and delete a specific key
+key = 'age'
+value = my_dict[key]
+del my_dict[key]
+print("Extracted value:", value)
+print("Updated dictionary:", my_dict)
+
+# Extract and delete all keys
+extracted_keys = list(my_dict.keys())
+for key in extracted_keys:
+    del my_dict[key]
+print("Extracted keys:", extracted_keys)
+print("Updated dictionary:", my_dict)
+
+# Restore the original dictionary
+my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
+
+# Extract and delete all values
+extracted_values = list(my_dict.values())
+my_dict.clear()
+print("Extracted values:", extracted_values)
+print("Updated dictionary:", my_dict)
+
+# Restore the original dictionary
+my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
+
+# Extract and delete all items
+extracted_items = list(my_dict.items())
+my_dict.clear()
+print("Extracted items:", extracted_items)
+print("Updated dictionary:", my_dict)
